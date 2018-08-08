a724492757cf6fa6ee88c41b9ed26d435aafd87d
adairaar <adairaar@gmail.com>
2018-08-08 09:55:57 -0400

    Add files via upload

--- server.R
+++ server.R
@@ -13,28 +13,28 @@ library(PerformanceAnalytics)
 library(xts)
 
 shinyServer(function(input, output) {
-   
-  Stock1 = NULL
-  Stock2 = NULL
   
   Data1 <- reactive({
-    Stock1 <- as.character(input$Stock1)
-    getSymbols(Stock1)
-    Stock1 <- getSymbols(as.character(input$Stock1))
+    Stock1 <- toString(input$Stock1)
+    #getSymbols(Stock1)
+    #Stock1 <- as.data.frame(getSymbols(Stock1))
   })
   
   Data2 <- reactive({
-    Stock2 <- as.character(input$Stock2)
-    getSymbols(Stock2)
-    Stock2 <- getSymbols(input$Stock2)
+    Stock2 <- toString(input$Stock2)
+    #getSymbols(Stock2)
+    #Stock2 <- getSymbols(Stock2)
   })
   
   output$distPlot <- renderPlot({
-    input$newplot
-    Stock1 <- AAPL
-    Stock2 <- GOOG
-    #Stock1 = Data1()
-    Data <- cbind(diff(log(Cl(Stock1))),diff(log(Cl(Stock2))))
+    Stock1 <- Data1()
+    Stock2 <- Data2()
+    #getSymbols(c(Data1(),Data2()))
+    dataEnv <- new.env()
+    getSymbols(c(Stock1,Stock2), env=dataEnv)
+    plist <- eapply(dataEnv, Ad)
+    pframe <- do.call(merge, plist)
+    Data <- cbind(diff(log((pframe))),diff(log((pframe))))
     chart.Correlation(Data)
     #plot(0, xaxt = 'n', yaxt = 'n', bty = 'n', pch = '', ylab = 'y', xlab = 'x')
   })
